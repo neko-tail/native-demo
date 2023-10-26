3fc06cf38b78480e761505985b01068a2449dd7f
xuqinwei <2711473070@qq.com>
2023-10-26 14:19:32 +0800

    group_concat test

--- src/test/java/com/example/nativedemo/NativeDemoApplicationTests.java
+++ src/test/java/com/example/nativedemo/NativeDemoApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.nativedemo;
 
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.toolkit.SqlRunner;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
 
 @SpringBootTest
 class NativeDemoApplicationTests {
@@ -16,6 +19,25 @@ class NativeDemoApplicationTests {
         clearData();
         Long count = userMapper.selectCount(Wrappers.emptyWrapper());
         assert count == 0;
+    }
+    
+    @Test
+    void testGroupConcat() {
+        clearData();
+        
+        String testNameString = "aaa,bbb,ccc";
+        String[] testNames = testNameString.split(",");
+        for (int i = 0; i < testNames.length; i++) {
+            userMapper.insert(new User(i, testNames[i]));
+        }
+        
+        String nameString = (String) SqlRunner.db().selectObj("select group_concat(`name`) from t_user order by id");
+        String[] names = nameString.split(",");
+        Arrays.sort(names);
+        Arrays.sort(testNames);
+        assert Arrays.equals(testNames, names);
+        
+        clearData();
     }
     
     private void clearData() {
