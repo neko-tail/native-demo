f986ffa3fb229c45adab6e84fa4b1e6d49b5d212
xuqinwei <2711473070@qq.com>
2023-10-26 14:23:23 +0800

    add test

--- src/test/java/com/example/nativedemo/NativeDemoApplicationTests.java
+++ src/test/java/com/example/nativedemo/NativeDemoApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.nativedemo;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NativeDemoApplicationTests {
     
+    @Autowired
+    private UserMapper userMapper;
+    
     @Test
     void contextLoads() {
+        clearData();
+        Long count = userMapper.selectCount(Wrappers.emptyWrapper());
+        assert count == 0;
+    }
+    
+    private void clearData() {
+        userMapper.delete(Wrappers.emptyWrapper());
     }
     
 }
